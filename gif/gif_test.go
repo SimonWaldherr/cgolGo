9d91ff850777d899a99ef4a8097f29dce134a5db
Simon Waldherr <git@simon.waldherr.eu>
2017-10-09 02:59:03 +0200

    add test for GIF gen do BCH stuff

---
+++ gif/gif_test.go
@@ -0,0 +1,15 @@
+package gif
+
+import (
+    "testing"
+)
+
+func Test_GIF(t *testing.T) {
+    var gv = &GifVisualizer{}
+    gv.Setup("tmp")
+    
+    gv.AddFrame([][]int{{0,1,0},{0,0,1},{1,1,1}})
+    gv.AddFrame([][]int{{1,0,1},{1,1,0},{0,0,0}})
+    gv.AddFrame([][]int{{0,1,0},{0,0,1},{1,1,1}})
+    gv.Complete()
+}
