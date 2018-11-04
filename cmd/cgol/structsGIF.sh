956d01cd7948537de3b32617687590bace2c8742
Simon Waldherr <git@simon.waldherr.eu>
2018-11-04 22:06:13 +0100

    add more RLE structures
    there is a huge collection of patterns:
    http://www.conwaylife.com/patterns/all.zip

--- cmd/cgol/structsGIF.sh
+++ cmd/cgol/structsGIF.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for i in $(seq -w 1 20) ; do 
-  go run main.go -w 50 -h 50 -d 250 -o "../../structures/$i.txt" -g $i -l 200;
-  gifsicle --resize-width 250 -O --careful -d 5 -o $i.gif $i.gif;
+i=0
+
+for filename in ../../structures/*; do
+  ((i++))
+  go run main.go -w 50 -h 50 -d 250 -o "$filename" -g $i -l 200;
+  gifsicle --change-color "#000000" "#FFFFFF" --change-color "#FFFFFF" "#000000" --resize-width 250 -O --careful -d 5 -o $i.gif $i.gif;
 done
