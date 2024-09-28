4d4d3e5b0506f167504e23d644ccbb35cf1200dd
Simon Waldherr <git@simon.waldherr.eu>
2024-09-28 18:30:30 +0200

    add Ebiten mode

---
+++ cmd/cgol/startEbiten.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "INSERT A FRAMERATE AND CONFIRM WITH \"ENTER\" [15]"
+read -s -t 30 FPS
+
+echo "INSERT AN AMOUNT OF FRAMES AND CONFIRM WITH \"ENTER\" [200]"
+read -s -t 30 FRAMES
+
+echo "INSERT A WIDTH AND CONFIRM WITH \"ENTER\" [250]"
+read -s -t 30 WIDTH
+
+echo "INSERT A HEIGHT AND CONFIRM WITH \"ENTER\" [250]"
+read -s -t 30 HEIGHT
+
+echo "PRESS \"R\" FOR A RANDOM MAP OR \"F\" TO LOAD FROM A FILE [R]"
+read -n 1 -s -t 30 INPUT
+
+
+
+if [ "$FPS" == "" ]
+  then
+  FPS=15
+fi
+
+if [ "$FRAMES" == "" ]
+  then
+  FRAMES=200
+fi
+
+if [ "$WIDTH" == "" ]
+  then
+  WIDTH=250
+fi
+
+if [ "$HEIGHT" == "" ]
+  then
+  HEIGHT=250
+fi
+
+if [ "$INPUT" == "" ]
+  then
+  INPUT="R"
+fi
+
+if [ "$INPUT" == "R" ] || [ "$INPUT" == "r" ]
+  then
+  go run main.go -e -w $((WIDTH/4)) -h $((HEIGHT/4)) -d $FRAMES -g cgol
+elif [ "$INPUT" == "F" ] || [ "$INPUT" == "f" ]
+  then
+  echo "ENTER THE NAME OF A FILE"
+  ls "../../structures"
+  read -n 2 -s INPUT
+  go run main.go -e -w $((WIDTH/4)) -h $((HEIGHT/4)) -d $FRAMES -g cgol -o "../../structures/$INPUT.txt"
+fi
--- go.mod
+++ go.mod
@@ -3,3 +3,13 @@ module simonwaldherr.de/go/cgolGo
 go 1.23.0
 
 require simonwaldherr.de/go/golibs v0.18.0
+
+require (
+	github.com/ebitengine/gomobile v0.0.0-20240911145611-4856209ac325 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.8.0 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
